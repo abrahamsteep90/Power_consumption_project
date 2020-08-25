0b45cfb99d880ecd886baf3eef3cd99e9de97c6e
abrahamsteep90 <67268095+abrahamsteep90@users.noreply.github.com>
2020-08-25 15:24:48 +0100

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,29 @@
+
+
+
+
+packages <- c("data.table", "reshape2")
+sapply(packages, require, character.only=TRUE, quietly=TRUE)
+path <- getwd()
+fileUrl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, file.path(path, "powerDt.zip"))
+unzip(zipfile = "powerDt.zip")
+powerDT<-data.table::fread(input = "household_power_consumption.txt", na.strings = "?")
+
+# Prevents histogram from printing in scientific notation
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+# Making a POSIXct date capable of being filtered and graphed by time of day
+powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+# Filter Dates for 2007-02-01 and 2007-02-02
+powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+png("plot2.png", width=480, height=480)
+
+## Plot 2
+plot(x = powerDT[, dateTime]
+     , y = powerDT[, Global_active_power]
+     , type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
